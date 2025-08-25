8c44de679d58c3fec42e32d507a3a2133c5d05c8
GulshanFin100x <gulshan@fin100x.ai>
2025-08-25 23:14:56 +0530

    removed middleware for testing

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -4,7 +4,7 @@ import { getQuiz, chat } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
-router.use(protectRoute);
+// router.use(protectRoute);
 
 router.get("/chat", chat);
 // router.post("/KYC/aadhar", onboardUser);
@@ -14,6 +14,7 @@ router.get("/quiz", getQuiz);
 // router.get("/profile", getUserDeatils);
 // router.get("/shorts", getShorts);
 // router.get("/videos", getvideos);
+// router.get("/news", getnews);
 
 
 export default router;
