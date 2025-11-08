364d26f59f63e4464155eecbad03458f531ea953
GulshanFin100x <gulshan@fin100x.ai>
2025-11-08 16:42:25 +0530

    status to true for API enableFeatues

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -25,7 +25,7 @@ router.get("/moneyrules/sections/:id/rules", getRulesBySection);
 
 router.get("/enableAllFeatures", (req, res) => {
   res.json({
-    status: false,
+    status: true,
   });
 });
 
